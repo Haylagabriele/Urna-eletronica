9df094db846d480a27817b7bea5d547286fe8101
Hayla Gabriele <haylafe7@hotmail.com>
2022-07-15 17:28:02 -0300

    confirm

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Urna-eletronica
+
---
+++ urna/etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+  {
+     titulo: 'VEREADOR',
+     numeros: 5,
+     candidatos: [
+     
+      {
+       numero: '38111',
+        nome: 'Fulano de tal',
+        partido: 'ABC',
+        fotos: [
+        {url: 'vbvb.jpg', legenda: 'Vereador'}
+          ]
+          
+      },
+  
+      {
+        numero: '77222',
+        nome: 'Beltrano de tal',
+        partido :'DEFG',
+        fotos:[
+          {url:'vice-pre.png', legenda:'vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'ciclano',
+        partido: 'ABC',
+        vice: 'cic',
+        fotos:[
+          {url: 'images.png',legenda:'prefeito'},
+          {url: 'jhjik.jpg', legenda:'Vice-Prefeito', small:true}
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'zulano',
+        partido: 'ABC',
+        vice: 'querti',
+        fotos: [
+          {url: 'kkk.png',legenda:'prefeito'},
+          {url: 'prefeito ss.jpg', legenda:'Vice-Prefeito', small:true}
+        ]
+      }
+    ]
+  }
+];
+
