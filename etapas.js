4fa23f59018ecf52bd23b72c537661767fb8b183
Hayla Gabriele <haylafe7@hotmail.com>
2022-08-23 14:49:59 -0300

    Mudança de pasta

--- etapas.js
+++ etapas.js
@@ -1,51 +1,51 @@
 let etapas = [
   {
-     titulo: 'VEREADOR',
+     titulo: 'Governador',
      numeros: 5,
      candidatos: [
      
       {
        numero: '38111',
-        nome: 'Fulano de tal',
+        nome: 'Pedro',
         partido: 'ABC',
         fotos: [
-        {url: 'vbvb.jpg', legenda: 'Vereador'}
+        {url: 'vbvb.jpg', legenda: 'Governador'}
           ]
           
       },
   
       {
         numero: '77222',
-        nome: 'Beltrano de tal',
+        nome: 'Sergio',
         partido :'DEFG',
         fotos:[
-          {url:'vice-pre.png', legenda:'vereador'}
+          {url:'vice-pre.png', legenda:'Governador'}
         ]
       },
     ]
   },
   {
-    titulo:'PREFEITO',
+    titulo:'Presidente',
     numeros: 2,
     candidatos: [
       {
         numero: '99',
-        nome: 'ciclano',
+        nome: 'Lula',
         partido: 'ABC',
-        vice: 'cic',
+        vice: 'Geraldo Alckmin',
         fotos:[
-          {url: 'images.png',legenda:'prefeito'},
-          {url: 'jhjik.jpg', legenda:'Vice-Prefeito', small:true}
+          {url: 'images.png',legenda:'Presidente'},
+          {url: 'jhjik.jpg', legenda:'Vice-Presidente', small:true}
         ]
       },
       {
         numero: '84',
-        nome: 'zulano',
-        partido: 'ABC',
-        vice: 'querti',
+        nome: 'Bolsonaro',
+        partido: 'CBA',
+        vice: 'General Braga Netto',
         fotos: [
-          {url: 'kkk.png',legenda:'prefeito'},
-          {url: 'prefeito ss.jpg', legenda:'Vice-Prefeito', small:true}
+          {url: 'kkk.png',legenda:'Presidente'},
+          {url: 'prefeito ss.jpg', legenda:'Vice-Presidente', small:true}
         ]
       }
     ]
